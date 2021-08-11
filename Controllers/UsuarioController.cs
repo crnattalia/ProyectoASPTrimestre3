b89001f9deb09ae387239413649c1e8912ef45e8
Natalia Castiblanco <castiblanconattalia@gmail.com>
2021-08-11 10:32:50 -0500

    CRUD completo de Usuario

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoASPTrimestre3.Models;
+
+namespace ProyectoASPTrimestre3.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        //Consultar tabla - retorna todos los usuarios
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities()) 
+            {
+             return View(db.usuario.ToList());
+            }
+            
+        }
+
+        //Mostrar formulario registrar un usuario
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Método para recibir la información de Create
+        //Registrar Usuario y guardar usuario 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(usuario usuario)
+        {
+            if (!ModelState.IsValid)
+                return View();
+        
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.usuario.Add(usuario);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return RedirectToAction("View"); 
+            }
+        }
+
+        //Detalles de usuario
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findUser = db.usuario.Find(id);
+                return View(findUser);
+            }
+        }
+
+        //Eliminar
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findUser = db.usuario.Find(id);
+                    db.usuario.Remove(findUser);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+
+        }
+
+        //Editar
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findUser = db.usuario.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser); 
+                }
+
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        //Recibir datos de editar
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(usuario editUser)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuario user = db.usuario.Find(editUser.id);
+                    user.nombre = editUser.nombre;
+                    user.apellido = editUser.apellido;
+                    user.email = editUser.email;
+                    user.fecha_nacimiento = editUser.fecha_nacimiento;
+                    user.password = editUser.password;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index"); 
+
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+    }
+}
