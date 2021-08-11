7c145417d1de9c675d5fcb698417404bba6e5a9e
Natalia Castiblanco <castiblanconattalia@gmail.com>
2021-08-11 16:23:40 -0500

    CRUD completo Proveedor

---
+++ Controllers/ProveedorController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoASPTrimestre3.Models;
+
+namespace ProyectoASPTrimestre3.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        // GET: Proveedor
+        //Consultar tabla - retorna todos los usuarios
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.proveedor.ToList());
+            }
+        }
+
+        //Mostrar formulario - registrar usuario
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.proveedor.Add(proveedor);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return RedirectToAction("View");
+            }
+        }
+
+        //Detalles de Proveedor
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findUser = db.proveedor.Find(id);
+                return View(findUser);
+            }
+        }
+
+        //Eliminar un registro
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findUser = db.proveedor.Find(id);
+                    db.proveedor.Remove(findUser);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        //Editar
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findUser = db.proveedor.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        //Recibir y guardar datos de editar
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit (proveedor editProveedor)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    proveedor proveedor = db.proveedor.Find(editProveedor.id);
+                    proveedor.nombre = editProveedor.nombre;
+                    proveedor.nombre_contacto = editProveedor.nombre_contacto;
+                    proveedor.telefono = editProveedor.telefono;
+                    proveedor.direccion = editProveedor.direccion;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+    }
+}
