4776755b902b6f5aca316f28ec032c062468f8f6
Natalia Castiblanco <castiblanconattalia@gmail.com>
2021-08-14 10:51:25 -0500

    CRUD de Cliente completo

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoASPTrimestre3.Models;
+
+namespace ProyectoASPTrimestre3.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        //Consultar tabla - retorna todos los usuarios
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.cliente.ToList());
+            }
+        }
+
+        //Registrar usuario
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create (cliente cliente)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return RedirectToAction("View");
+            }
+        }
+
+        //Detalles de cliente
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findUser = db.cliente.Find(id);
+                return View(findUser);
+            }
+        }
+
+        //Eliminar registro
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findUser = db.cliente.Find(id);
+                    db.cliente.Remove(findUser);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        //Editar registros
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findUser = db.cliente.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        //Recibir y guardar datos editados
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cliente editCliente)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    cliente cliente = db.cliente.Find(editCliente.id);
+                    cliente.nombre = editCliente.nombre;
+                    cliente.documento = editCliente.documento;
+                    cliente.email = editCliente.email;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+    }
+}
