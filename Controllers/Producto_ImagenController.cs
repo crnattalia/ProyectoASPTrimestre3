76327d6ee13e6e6fa8578bf3f0fb0078fbb186f5
Natalia Castiblanco <castiblanconattalia@gmail.com>
2021-08-21 20:04:38 -0500

    CRUD completo de Producto_Imagen, vista parcial, vistas. CRUD Usuariorol con errores

---
+++ Controllers/Producto_ImagenController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoASPTrimestre3.Models;
+
+namespace ProyectoASPTrimestre3.Controllers
+{
+    public class Producto_ImagenController : Controller
+    {
+        // GET: Producto_Imagen
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.producto_imagen.ToList());
+            }
+        }
+
+        public static string nombreProducto(int idProducto)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.producto.Find(idProducto).nombre;
+            }
+        }
+
+        public ActionResult ListarProducto()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.producto.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(producto_imagen producto_Imagen)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.producto_imagen.Add(producto_Imagen);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError(" ", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    return View(db.producto_imagen.Find(id));
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError(" ", "Error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                producto_imagen iproductoEdit = db.producto_imagen.Where(a => a.id == id).FirstOrDefault();
+                return View(iproductoEdit);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(producto_imagen iproductoEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var oldiProducto = db.producto_imagen.Find(iproductoEdit.id);
+                    oldiProducto.id_producto = iproductoEdit.id_producto;
+                    oldiProducto.imagen = iproductoEdit.imagen;
+                    oldiProducto.id = iproductoEdit.id;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError(" ", "Error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    producto_imagen producto_imagen = db.producto_imagen.Find(id);
+                    db.producto_imagen.Remove(producto_imagen);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError(" ", "Error " + ex);
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/UsuarioRolController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoASPTrimestre3.Models;
+
+namespace ProyectoASPTrimestre3.Controllers
+{
+    public class UsuarioRolController : Controller
+    {
+        // GET: UsuarioRol
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.usuariorol.ToList());
+            }
+        }
+
+        public static string nombreUsuario(int idUsuario)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.usuario.Find(idUsuario).nombre;
+            }
+        }
+
+        public ActionResult ListarUsuario()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.usuario.ToList());
+            }
+        }
+
+        public static string DescripcionRol(int idRol)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.roles.Find(idRol).descripcion;
+            }
+        }
+
+        public ActionResult ListarDescripcionRol()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.roles.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(usuariorol usuariorol)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.usuariorol.Add(usuariorol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(" ", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                using (var db =new inventario2021Entities())
+                {
+                    return View(db.usuariorol.Find(id));
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                usuariorol rolEdit = db.usuariorol.Where(a => a.id == id).FirstOrDefault();
+                return View(rolEdit);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(usuariorol rolEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var oldrol = db.usuariorol.Find(rolEdit.id);
+                    oldrol.idUsuario = rolEdit.idUsuario;
+                    oldrol.idRol = rolEdit.idRol;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuariorol usuariorol = db.usuariorol.Find(id);
+                    db.usuariorol.Remove(usuariorol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+    }
+}
