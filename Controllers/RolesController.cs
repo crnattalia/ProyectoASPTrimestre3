ea236dff0ccf2aecba613d831a5fef47455b0d74
Natalia Castiblanco <castiblanconattalia@gmail.com>
2021-09-28 01:17:25 -0500

    Cargue masivo de datos Cliente y Roles

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -121,6 +121,71 @@ namespace ProyectoASPTrimestre3.Controllers
             }
         }
 
+        public ActionResult uploadCSV()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult uploadCSV(HttpPostedFileBase fileForm)
+        {
+            try
+            {
+
+                //string para guardar la ruta
+                string filePath = string.Empty;
+
+                //condicion para saber si el archivo llego
+                if (fileForm != null)
+                {
+                    //ruta de la carpeta que guardara el archivo
+                    string path = Server.MapPath("~/Uploads/");
+
+                    //condicion para saber si la carpeta uploads existe
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    //obtener el nombre del archivo
+                    filePath = path + Path.GetFileName(fileForm.FileName);
+
+                    //obtener la extension del archivo
+                    string extension = Path.GetExtension(fileForm.FileName);
+
+                    //guardar el archivo
+                    fileForm.SaveAs(filePath);
+
+                    string csvData = System.IO.File.ReadAllText(filePath);
+
+                    foreach (string row in csvData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            var newrol = new roles
+                            {
+                                descripcion = row.Split(';')[0],
+                            };
+
+                            using (var db = new inventario2021Entities())
+                            {
+                                db.roles.Add(newrol);
+                                db.SaveChanges();
+                            }
+                        }
+                    }
+                }
+
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
         public ActionResult PaginadorIndex(int pagina = 1)
         {
             try
